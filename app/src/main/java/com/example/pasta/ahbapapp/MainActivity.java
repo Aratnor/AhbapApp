5745400f81c1e3ad184dddedbc0ebc50029b13ce
Mehmet Ali Ocak <ocakmali@gmail.com>
2018-03-24 15:31:25 +0300

    add google sign in

--- app/src/main/java/com/example/pasta/ahbapapp/MainActivity.java
+++ app/src/main/java/com/example/pasta/ahbapapp/MainActivity.java
@@ -1,12 +1,43 @@
 package com.example.pasta.ahbapapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import com.example.pasta.ahbapapp.Login.LoginActivity;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
+
+private FirebaseAuth mAuth;
 
     @Override protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mAuth = FirebaseAuth.getInstance();
+        final FirebaseUser currentUser = mAuth.getCurrentUser();
+
+        findViewById(R.id.sign_out).setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                Intent i = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(i);
+            }
+        });
+
+        if (currentUser != null){
+            TextView t = findViewById(R.id.textView);
+            t.setText(currentUser.getDisplayName());
+        }
+
+
+        if(currentUser == null){
+            Intent intent = new Intent(this, LoginActivity.class);
+            startActivity(intent);
+        }
+
     }
 }
