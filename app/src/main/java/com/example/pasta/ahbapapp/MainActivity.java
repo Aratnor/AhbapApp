47bb2f707dc600726a255cc053f9fbcb6cd74dbe
Mehmet Ali Ocak <ocakmali@gmail.com>
2018-03-26 19:50:08 +0300

    deleted facebook auth

--- app/src/main/java/com/example/pasta/ahbapapp/MainActivity.java
+++ app/src/main/java/com/example/pasta/ahbapapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.pasta.ahbapapp;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
@@ -9,6 +10,9 @@ import android.widget.TextView;
 import android.widget.Toast;
 import com.example.pasta.ahbapapp.login.LoginActivity;
 import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.firebase.auth.FirebaseAuth;
@@ -16,18 +20,28 @@ import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
-private FirebaseAuth mAuth;
-private FirebaseAuth.AuthStateListener mAuthStateListener;
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
+    private GoogleSignInClient mGoogleSignInClient;
 
+    @SuppressLint("RestrictedApi")
     @Override protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         mAuth = FirebaseAuth.getInstance();
 
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build();
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
         findViewById(R.id.sign_out).setOnClickListener(new View.OnClickListener() {
             @Override public void onClick(View v) {
                 mAuth.signOut();
+                mGoogleSignInClient.signOut();
             }
         });
 
