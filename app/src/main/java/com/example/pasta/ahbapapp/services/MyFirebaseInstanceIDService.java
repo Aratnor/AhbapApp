e970a4f382679ff2de0ee2b67705b13de4a68e84
Aratnor <tunabolat@anadolu.edu.tr>
2018-04-09 19:52:12 +0300

    Notification Services classes added

---
+++ app/src/main/java/com/example/pasta/ahbapapp/services/MyFirebaseInstanceIDService.java
@@ -0,0 +1,17 @@
+package com.example.pasta.ahbapapp.services;
+
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+public class MyFirebaseInstanceIDService extends FirebaseInstanceIdService {
+    private static String TAG = "Registration";
+
+    @Override
+    public void onTokenRefresh() {
+        // Get updated InstanceID token.
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        Log.d(TAG, "Refreshed token: " + refreshedToken);
+
+        // TODO: Implement this method to send any registration to your app's servers.
+        System.out.println("Registration.onTokenRefresh TOKEN: " + refreshedToken );
+}
+}
