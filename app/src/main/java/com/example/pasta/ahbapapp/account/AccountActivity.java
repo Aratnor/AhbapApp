be5a52fb7ea1f9e7cb77411e755261e085cb5a0d
Aratnor <tunabolat@anadolu.edu.tr>
2018-04-18 22:07:21 +0300

    Updated account activity

---
+++ app/src/main/java/com/example/pasta/ahbapapp/account/AccountActivity.java
@@ -0,0 +1,15 @@
+package com.example.pasta.ahbapapp.account;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.pasta.ahbapapp.R;
+
+public class AccountActivity extends AppCompatActivity {
+
+}
