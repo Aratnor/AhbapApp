8448581c0e5b61bb4a6134a2811e5ef27f28ba2e
Aratnor <tunabolat@anadolu.edu.tr>
2018-05-03 19:38:02 +0300

    Added MessageActivity

---
+++ app/src/main/java/com/example/pasta/ahbapapp/chat/message/MessageActivity.java
@@ -0,0 +1,15 @@
+package com.example.pasta.ahbapapp.chat.message;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.pasta.ahbapapp.R;
+
+public class MessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_message);
+    }
+}
