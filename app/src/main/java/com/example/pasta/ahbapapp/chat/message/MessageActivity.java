1aac30a9a7a72990c7aabbfc550cf83b28ed00ee
Aratnor <tunabolat@anadolu.edu.tr>
2018-05-04 20:13:12 +0300

    Updated Chat Database

--- app/src/main/java/com/example/pasta/ahbapapp/chat/message/MessageActivity.java
+++ app/src/main/java/com/example/pasta/ahbapapp/chat/message/MessageActivity.java
@@ -15,6 +15,7 @@ import com.example.pasta.ahbapapp.MainActivity;
 import com.example.pasta.ahbapapp.R;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.Date;
@@ -22,16 +23,16 @@ import java.util.HashMap;
 import java.util.Objects;
 
 import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
-public class MessageActivity extends AppCompatActivity implements View.OnClickListener {
+public class MessageActivity extends AppCompatActivity {
     @BindView(R.id.message)
     EditText message;
     @BindView(R.id.messageButton)
     ImageButton messageButton;
-    @BindView(R.id.message_list)
-    RecyclerView messageList;
 
-    private final String TAG = "Info";
+    private final String TAG = "MEssageActivity";
 
     private String messageUserId;
     private String currentUserId;
@@ -43,41 +44,76 @@ public class MessageActivity extends AppCompatActivity implements View.OnClickLi
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_message);
+        ButterKnife.bind(this);
 
-        messageUserId = Objects.requireNonNull(getIntent().getExtras()).getString("userID");
+        db = FirebaseFirestore.getInstance();
+
+        messageUserId = getIntent().getExtras().getString("userID");
         sharedPreferences = getSharedPreferences("com.example.pasta.ahbapapp"
                 , Context.MODE_PRIVATE);
         currentUserId = sharedPreferences.getString(MainActivity.USER_ID,"");
     }
 
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.messageButton :
-                if(!currentUserId.isEmpty()) {
-                    String context = message.getText().toString();
-                    Date timeStamp = new Date();
+
+
+    @OnClick(R.id.messageButton)
+    public void sendMessage() {
+        Log.i(TAG,"Button Clicked");
+        if(!currentUserId.isEmpty()) {
+            final String context = message.getText().toString();
+            final Date timeStamp = new Date();
+            HashMap<String,Object> map = new HashMap<>();
+            map.put("message",context);
+            map.put("timeStamp",timeStamp);
+            map.put("sender",true);
+            db.collection("message").document(currentUserId).collection(messageUserId).add(map).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                @Override
+                public void onSuccess(DocumentReference documentReference) {
+                    Log.i(TAG,"Message successfully added to currentUser Database");
+                }
+            });
+            map.put("sender",false);
+            db.collection("message").document(messageUserId).collection(currentUserId).add(map).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                @Override
+                public void onSuccess(DocumentReference documentReference) {
+                    Log.i(TAG,"Message successfully added to messageUser Database");
+                }
+            });
+            db.collection("chat").document(currentUserId).collection("chats").document(messageUserId).get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                @Override
+                public void onSuccess(DocumentSnapshot documentSnapshot) {
                     HashMap<String,Object> map = new HashMap<>();
-                    map.put("message",context);
-                    map.put("timeStamp",timeStamp);
-                    map.put("sender",true);
-                    db.collection("message").document(currentUserId).collection(messageUserId).add(map).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
-                        @Override
-                        public void onSuccess(DocumentReference documentReference) {
-                            Log.i(TAG,"Message successfully added to currentUser Database");
-                        }
-                    });
-                    map.put("sender",false);
-                    db.collection("message").document(messageUserId).collection(currentUserId).add(map).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
-                        @Override
-                        public void onSuccess(DocumentReference documentReference) {
-                            Log.i(TAG,"Message succesfully added to messageUser Database");
-                        }
-                    });
+                    map.put("last_message", context);
+                    map.put("updated_at",timeStamp);
+                    DocumentReference chat = documentSnapshot.getReference();
+                    if(documentSnapshot.exists()){
+                        chat.update(map);
+                    }
+                    else {
+                        chat.set(map);
+                    }
                 }
-                else {
-                    Log.i(TAG,"userid is null from sharedPref");
+            });
+            db.collection("chat").document(messageUserId).collection("chats").document(currentUserId).get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                @Override
+                public void onSuccess(DocumentSnapshot documentSnapshot) {
+                    HashMap<String,Object> map = new HashMap<>();
+                    map.put("last_message", context);
+                    map.put("updated_at",timeStamp);
+                    DocumentReference chat = documentSnapshot.getReference();
+                    if(documentSnapshot.exists()){
+                        chat.update(map);
+                    }
+                    else {
+                        chat.set(map);
+                    }
                 }
+            });
         }
+        else {
+            Log.i(TAG,"UserID is null from sharedPref");
+        }
+
     }
+
 }
